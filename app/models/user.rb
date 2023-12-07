e839879a71235a9d0d8e24ba880ec703aa288996
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2023-12-07 18:48:39 +0000

    devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  #config/initializers/devise.rbではなくAdminとUserモデルに別々で
+  #authentication_keys: [:値]を記述することでAdminの場合はemailでuserの場合は
+  #nameでログインできるようになる
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, authentication_keys: [:name]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 会員用
+  #devise_for :テーブル名でURLを変更
+  #skip:パスワード変更のコントローラを削除
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+
+  #scope moduleと付けることでURLとファイル構成を指定
+  #URLのパスは変わらず、Controller#Actionの表記がuser/resorces/アクションになる
+  scope module: :user do
+    # User関連
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+  # 管理者用
+  ##devise_for :テーブル名でURLを変更
+  #skip:で新規登録とパスワード変更のコントローラを削除
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #namespaceはURLとファイル構成を指定
+  namespace :admin do
+    # Admin関連
+  end
+
+  # root "homes#top"
 end
