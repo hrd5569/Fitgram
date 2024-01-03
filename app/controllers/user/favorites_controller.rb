0e6fb027c6fe540381f6a0105b07a8b12454c36c
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2024-01-03 17:27:54 +0000

    非同期

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,22 +1,30 @@
 class User::FavoritesController < ApplicationController
-
   before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:create, :destroy]
 
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   def destroy
-      current_user.favorites.destroy_by(id: params[:id])
-      redirect_back(fallback_location: root_path)
+    favorite = current_user.favorites.find_by(post_id: params[:post_id])
+    favorite.destroy if favorite
+    @post = Post.find(params[:post_id])
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
-end
 
   private
 
   def ensure_guest_user
     redirect_to root_path, notice: "ゲストユーザーのため実行できません。" if current_user.email == "guest@example.com"
   end
+end
