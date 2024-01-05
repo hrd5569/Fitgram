c0192c92ca5a5aee982f9dffca41f578209211b4
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2024-01-05 03:34:33 +0000

    レイアウト

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -3,22 +3,22 @@ class User::FavoritesController < ApplicationController
   before_action :ensure_guest_user, only: [:create, :destroy]
 
   def create
-    @post = Post.find(params[:post_id])
+      @post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
     respond_to do |format|
       format.html { redirect_back(fallback_location: root_path) }
-      format.js
+      format.js # JavaScript の応答を追加
     end
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(post_id: params[:post_id])
+      favorite = current_user.favorites.find_by(post_id: params[:post_id])
     favorite.destroy if favorite
     @post = Post.find(params[:post_id])
     respond_to do |format|
       format.html { redirect_back(fallback_location: root_path) }
-      format.js
+      format.js # JavaScript の応答を追加
     end
   end
 
