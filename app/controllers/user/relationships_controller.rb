cf55e5ba626d66c92d298ad4360064505e53bbbc
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2024-01-19 14:45:58 +0000

    非同期通信

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -3,16 +3,22 @@ class User::RelationshipsController < ApplicationController
 
   # フォローする
   def create
-    other_user = User.find(params[:id])
-    current_user.follow(other_user)
-    redirect_back(fallback_location: root_path)
+    @other_user = User.find(params[:id])
+    current_user.follow(@other_user)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   # フォロー解除する
   def destroy
-    other_user = User.find(params[:id])
-    current_user.unfollow(other_user)
-    redirect_back(fallback_location: root_path)
+    @other_user = User.find(params[:id])
+    current_user.unfollow(@other_user)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   private
